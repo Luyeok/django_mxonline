9d79f42a5e01f6e5b29dc3d308c6e6985a9de29f
LY <lyinfo@tacpark.com>
2019-03-19 23:06:41 +0800

    20190319

--- apps/users/models.py
+++ apps/users/models.py
@@ -10,11 +10,14 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     nick_name=models.CharField(max_length=50, verbose_name='昵称',default='')
     birthday=models.DateField(verbose_name=u'生日',null=True, blank=True)
-    gender=models.CharField(choices=(("Maile","男"),("female","女")),
-                            default="female",max_length=5)
-    address=models.CharField(max_length=100,default="")
-    mobile=models.CharField(max_length=11, null=True, blank=True)
-    image=models.ImageField(upload_to="image/%Y/%m",default="image/default.png", max_length=100)
+    gender=models.CharField(choices=(("Male","男"),("Female","女")),
+                            default="female",max_length=7,verbose_name="性别")
+    address=models.CharField(max_length=100,default="",verbose_name="地址")
+    mobile=models.CharField(max_length=11, null=True,
+                            blank=True, verbose_name="手机")
+    image=models.ImageField(upload_to="image/%Y/%m",
+                            default="image/default.png", max_length=100,
+                            verbose_name="头像")
     class Meta:
         verbose_name="用户信息"
         verbose_name_plural=verbose_name
