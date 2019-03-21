16f19e910954b21a130f8583f06843903b7f03f9
LY <lyinfo@tacpark.com>
2019-03-21 21:53:58 +0800

    20190321

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
 
-from .models import UserProfile
 
-
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(UserProfile, UserProfileAdmin)
